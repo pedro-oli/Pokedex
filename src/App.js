1c7eb6663dd1cb90dd7ef871b2102a4e16d3099c
Pedro Oliveira <pedro.oli.95@outlook.com>
2019-11-24 20:11:25 -0300

    Added search; Added 'type' filter

--- src/App.js
+++ src/App.js
@@ -6,24 +6,46 @@ class App extends React.Component {
         super();
         this.state = {
             pokemons: [],
+            filteredPokemons: [],
             currentPage: 1,
-            pokemonsPerPage: 8
+            pokemonsPerPage: 8,
+            typeOptions: []
         };
-        this.getAPIData()
         this.handleClick = this.handleClick.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
+        this.handleType = this.handleType.bind(this);
+    }
+
+    componentDidMount() {
+        this.getAPIData()
     }
 
     getAPIData() {
         var pokemons = []
         fetch(
-            'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151' , { method: 'GET' }
+            'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151', { method: 'GET' }
         ).then(
             results => results.json()
         ).then(
             data => {
                 data.results.forEach((pok, index) => {
-                    pokemons.push({id: index+1, name: pok.name})
-                    this.setState({ pokemons })
+                    fetch(
+                        pok.url, { method: 'GET' }
+                    ).then(
+                        results => results.json()
+                    ).then(
+                        data => {
+                            var types = data.types.map(type => type.type.name)
+                            pokemons.push({
+                                id: index+1,
+                                name: pok.name,
+                                imageURL: data.sprites.front_default,
+                                types: types
+                            })
+                            pokemons.sort((a, b) => a.id > b.id)
+                            this.setState({ pokemons, filteredPokemons: pokemons })
+                        }
+                    )
                 })
             }
         )
@@ -35,22 +57,48 @@ class App extends React.Component {
         });
     }
 
+    handleSearch(event) {
+        // set no type filters
+        document.getElementById("select-field").getElementsByTagName('option')[0].selected = 'selected'
+
+        var filteredPokemons = this.state.pokemons;
+        filteredPokemons = filteredPokemons.filter(function(pok) {
+            return pok.name.search(event.target.value.toLowerCase()) !== -1;
+        });
+        this.setState({ currentPage: 1, filteredPokemons });
+    }
+
+    handleType(event) {
+        // set no name search filter
+        document.getElementById("search-field").value = ""
+
+        var filteredPokemons = this.state.pokemons;
+        if (event.target.value !== "") {
+            filteredPokemons = filteredPokemons.filter(function(pok) {
+                return pok.types.includes(event.target.value)
+            });
+        }
+        this.setState({ currentPage: 1, filteredPokemons });
+    }
+
     render() {
-        const { pokemons, currentPage, pokemonsPerPage } = this.state;
+        const { currentPage, pokemonsPerPage, filteredPokemons } = this.state;
 
         const indexOfLastPokemon = currentPage * pokemonsPerPage;
         const indexOfFirstPokemon = indexOfLastPokemon - pokemonsPerPage;
-        const currentPokemons = pokemons.slice(indexOfFirstPokemon, indexOfLastPokemon);
-
-        const renderPokemons = currentPokemons.map((pok, index) => {
-            return <li key={index}>{pok.id} - {pok.name}</li>;
-        });
-
+        const currentPokemons = filteredPokemons.slice(indexOfFirstPokemon, indexOfLastPokemon);
         const pageNumbers = [];
-        for (let i = 1; i <= Math.ceil(pokemons.length / pokemonsPerPage); i++) {
+        for (let i = 1; i <= Math.ceil(filteredPokemons.length / pokemonsPerPage); i++) {
             pageNumbers.push(i);
         }
 
+        const renderPokemons = currentPokemons.map((pok, index) => {
+            return <li key={index}>
+                <img alt={pok.name} src={pok.imageURL} />
+                <p>{pok.id} - {pok.name}</p>
+            </li>;
+        });
+        
         const renderPageNumbers = pageNumbers.map(number => {
             return (
                 <li
@@ -63,9 +111,43 @@ class App extends React.Component {
             );
         });
 
+        const selectTypes = (
+            <select id="select-field" defaultValue="" onChange={this.handleType}>
+                <option value="">Select Type</option>
+                <option value="poison">Poison</option>
+                <option value="grass">Grass</option>
+                <option value="fire">Fire</option>
+                <option value="flying">Flying</option>
+                <option value="water">Water</option>
+                <option value="bug">Bug</option>
+                ​<option value="normal">Normal</option>
+                ​<option value="electric">Electric</option>
+                <option value="ground">Ground</option>
+                <option value="fairy">Fairy</option>
+                <option value="fighting">Fighting</option>
+                ​<option value="psychic">Psychic</option>
+                <option value="rock">Rock</option>
+                ​<option value="steel">Steel</option>
+                ​<option value="ice">Ice</option>
+                ​<option value="ghost">Ghost</option>
+                ​<option value="dragon">Dragon</option>
+            </select>
+        )
+
+        const searchField = (
+            <input
+                id="search-field"
+                type="text"
+                placeholder="Search Pokémon"
+                onChange={this.handleSearch}
+            />
+        )
+
         return (
             <div>
-                <ul>
+                {searchField}
+                {selectTypes}
+                <ul id="pokemon-list">
                     {renderPokemons}
                 </ul>
                 <ul id="page-numbers">
